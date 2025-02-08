8682a76186dad2396635727f3221b9eb2c954a4e
Chino Nanami <tino_nanami@7mi.site>
2025-02-08 14:50:44 +0900

    config.ymlを生成するように

--- src/main/java/net/nicovrc/dev/Main.java
+++ src/main/java/net/nicovrc/dev/Main.java
@@ -1,17 +1,78 @@
 package net.nicovrc.dev;
 
 
+import com.amihaiemil.eoyaml.Yaml;
+import com.amihaiemil.eoyaml.YamlMapping;
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        if (!new File("./log").exists()){
-            new File("./log").mkdir();
+        int port = Function.HTTPPort;
+        String FolderPass = "./log";
+
+        try {
+            if (new File("./config.yml").exists()){
+                try {
+                    final YamlMapping yamlMapping = Yaml.createYamlInput(new File("./config.yml")).readYamlMapping();
+                    port = yamlMapping.integer("Port");
+                    FolderPass = yamlMapping.string("LogFileFolderPass");
+                } catch (Exception e){
+                    // e.printStackTrace();
+                }
+            } else {
+
+                String config = """
+# ----------------------------
+#
+# 基本設定
+#
+# ----------------------------
+# 受付ポート (HTTP/UDP共通)
+Port: 25555
+# ログをRedisに書き出すときはtrue
+LogToRedis: false
+# (Redis使わない場合)ログの保存先
+LogFileFolderPass: "./log"
+# ----------------------------
+#
+# Redis設定
+# ※ログをRedisに保存する際に使う
+#
+# ----------------------------
+# Redisサーバー
+RedisServer: "127.0.0.1"
+RedisPort: 6379
+# Redis AUTHパスワード
+# パスワードがない場合は以下の通りに設定してください
+RedisPass: ""
+                    """;
+
+                FileWriter file = new FileWriter("./config.yml");
+                PrintWriter pw = new PrintWriter(new BufferedWriter(file));
+                pw.print(config);
+                pw.close();
+                file.close();
+
+                System.out.println("[Info] config.ymlを設定してください");
+                return;
+            }
+
+            if (!new File(FolderPass).exists()){
+                new File(FolderPass).mkdir();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            return;
         }
 
-        Thread httpServer = new HTTPServer();
+        Thread httpServer = new HTTPServer(port);
         httpServer.start();
     }
 
