bfdc4989a48d4aa74659b67163477a7585222491
Chino Nanami <tino_nanami@7mi.site>
2026-07-10 22:08:13 +0900

    fix

--- src/main/java/net/nicovrc/dev/HTTPServer.java
+++ src/main/java/net/nicovrc/dev/HTTPServer.java
@@ -250,7 +250,7 @@ public class HTTPServer extends Thread {
                     public void completed(AsynchronousSocketChannel ch, Void att) {
                         server.accept(null, this);
 
-                        ByteBuffer buf = ByteBuffer.allocate(2048);
+                        ByteBuffer buf = ByteBuffer.allocate(1024768*32);
                         ch.read(buf, buf, new CompletionHandler<>() {
                             public void completed(Integer n, ByteBuffer b) {
                                 if (n == -1) {
